9c3979773665aa7035233845a950f789ff707603
mumtazalwan <mumtazalwan@gmail.com>
2023-12-18 19:45:16 +0700

    feat: province

---
+++ database/migrations/2023_12_18_120241_create_provinces_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('provinces', function (Blueprint $table) {
+            $table->id();
+            $table->string('province_name');
+            $table->string('capital');
+            $table->string('local_language');
+            $table->string('island');
+            $table->integer('population');
+            $table->string('ethnic');
+            $table->string('regional_house');
+            $table->string('regional_house_image');
+            $table->string('dance');
+            $table->string('dance_image');
+            $table->string('weapon');
+            $table->string('weapon_image');
+            $table->string('music');
+            $table->string('music_image');
+            $table->string('tradition');
+            $table->text('flash_info');
+            $table->text('more_detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('provinces');
+    }
+};
