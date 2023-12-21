a1ca87335eb15cdce7e10dd0c7e64de552f2b117
mumtazalwan <mumtazalwan@gmail.com>
2023-12-21 22:53:37 +0700

    apalah

---
+++ app/Http/Controllers/DestinationController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Destination;
+use Illuminate\Http\Request;
+
+class DestinationController extends Controller
+{
+    public function index() {
+        $data = Destination::all();
+
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'data' => $data??[]
+        ]);
+    }
+
+    public function discovery() {
+        $data = Destination::whereNotNull('banner')->get();
+
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'data' => $data??[]
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DestinationController;
+use App\Http\Controllers\FunfactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/carousel/discovery', [DestinationController::class, 'discovery']);
+Route::get('/destination', [DestinationController::class, 'index']);
+Route::get('/province/{id}');
+Route::get('/kuliner');
+Route::get('/funfact', [FunfactController::class, 'getFunFact']);
